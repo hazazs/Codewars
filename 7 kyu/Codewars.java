d47e6d88f8a580c7970bfe958717b0b45c1d8fb8
hazazs <hazafi.tibor@gmail.com>
2021-03-22 13:19:51 +0100

    Codewars

---
+++ 7 kyu/Codewars.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codewars {
+
+    /*
+    An English Twist on a Japanese Classic
+     */
+    public static List<String> theGame(List<String> words) {
+        int i = 0;
+        while (i < words.size() && isValidWord(words, i)) {
+            i++;
+        }
+        return words.subList(0, i);
+    }
+    public static boolean isValidWord(List<String> w, int i) {
+        return !w.get(i).isEmpty() && (i == 0 || w.get(i - 1).endsWith(String.valueOf(w.get(i).charAt(0))));
+    }
+
+    /*
+    Descending Order
+     */
+    public static int sortDesc(final int num) {
+        int tmp = num;
+        List<Integer> list = new ArrayList<>();
+        while (tmp != 0) {
+            list.add(tmp % 10);
+            tmp /= 10;
+        }
+        list.sort((num1, num2) -> Integer.compare(num1, num2));
+        int max = 0;
+        for (int i = 0; i < list.size(); i++)
+            max += list.get(i) * Math.pow(10, i);
+        return max;
+    }
+
+    /*
+    Square Every Digit
+     */
+    public int squareDigits(int n) {
+        StringBuilder builder = new StringBuilder(String.valueOf(n).length());
+        while (n != 0) {
+            builder.insert(0, Math.round(Math.pow(n%10, 2)));
+            n /= 10;
+        }
+        return Integer.parseInt(builder.toString());
+    }
+    
+}
