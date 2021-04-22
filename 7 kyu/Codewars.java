7c1feeb8668ede68daf200ba680c2c6925f60220
hazazs <hazafi.tibor@gmail.com>
2021-04-22 15:32:39 +0200

    Codewars

--- 7 kyu/Codewars.java
+++ 7 kyu/Codewars.java
@@ -176,4 +176,16 @@ public class Codewars {
         return coor;
     }
 
+    /*
+    Count the Digit
+     */
+    public static int nbDig(int n, int d) {
+        int count = 0;
+        for (int i = 0; i <= n; i++)
+            count += String.valueOf(i * i).codePoints()
+                    .filter(ch -> ch == Character.forDigit(d, 10))
+                    .count();
+        return count;
+    }
+
 }
