af41d3ad211dd74a4bd26c4578fb1fb1cbd7e643
hazazs <hazafi.tibor@gmail.com>
2021-04-27 13:52:51 +0200

    Codewars

--- 7 kyu/Codewars.java
+++ 7 kyu/Codewars.java
@@ -1,7 +1,9 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -201,6 +203,25 @@ public class Codewars {
                 .mapToObj(i -> s.substring(i, i + 1).repeat(i + 1))
                 .map(str -> str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase())
                 .collect(Collectors.joining("-"));
+    }
+    
+    /*
+    Moves in squared strings (I)
+     */
+    public static String vertMirror(String strng) {
+        return Arrays.stream(strng.split("\n"))
+                .map(str -> new StringBuilder(str).reverse().toString())
+                .collect(Collectors.joining("\n"));
+    }
+    
+    public static String horMirror(String strng) {
+        return Arrays.stream(strng.split("\n"))
+                .sorted((str1, str2) -> -1)
+                .collect(Collectors.joining("\n"));
+    }
+    
+    public static String oper(UnaryOperator<String> operator, String s) {
+        return operator.apply(s);
     }
 
 }
